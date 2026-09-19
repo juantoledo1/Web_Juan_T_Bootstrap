5e66a5171139797ba9eb1fcc35cd48cf050cca31
juantoledo1 <juanmanueltoledo0487@gmail.com>
2026-09-19 12:47:37 -0300

    chore(deps): upgrade astro to 7.3.3 and add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  // Opciones adicionales aquí si es necesario
+  site: 'https://betelgeusee.netlify.app',
+  integrations: [sitemap()],
 });
